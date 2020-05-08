9fb00ef8cec409bdeeea9dc9471058f2f145c501
MatejFrnka <mat.frnka@gmail.com>
2020-05-08 11:42:16 +0200

    Fixed makefile

---
+++ Utility/EventSet.h
@@ -0,0 +1,21 @@
+/**
+ * @author: Matej Frnka <frnkamat@fit.cvut.cz>
+ * @date: 03.05.2020
+ */
+
+#ifndef CALENDAR_UTILITY_EVENT_SET
+#define CALENDAR_UTILITY_EVENT_SET
+
+#include <set>
+
+template<typename T>
+struct compare_reference {
+    bool operator()(const T lhs, const T rhs) {
+        return *lhs < *rhs;
+    }
+};
+
+template<typename T>
+using EventSet = std::set<T, compare_reference<T>>;
+
+#endif
